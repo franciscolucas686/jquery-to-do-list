c969047ebd241a3a1253f67e377f88dcba64e818
Francisco Lucas <franciscolucas686@gmail.com>
2024-03-05 21:44:08 -0300

    Fix: bug ao selecionar as li da lista

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     $('form').on('submit', function (e) {
         e.preventDefault();
         const novaTarefa = $('#nome-tarefa').val();
-        const novoItem = $(`<li class ="nao-finalizado">${novaTarefa}</li>`);
+        const novoItem = $(`<li>${novaTarefa}</li>`);
 
         $(novoItem).appendTo('ul');
         $('#nome-tarefa').val('');
 
-    });
-    $('ul').click('li', function () {
-        $(this).toggleClass('finalizado')
+        $('ul li').click(function(){
+            $(this).toggleClass('finalizado')
+        })
     });
 });
